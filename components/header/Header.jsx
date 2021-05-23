32b48d0f36a1c209ba7210ba6dcea91e556a0d23
Mungun2227 <78751955+Mungun2227@users.noreply.github.com>
2021-05-23 16:34:50 +0800

    Update Header.jsx

--- components/header/Header.jsx
+++ components/header/Header.jsx
@@ -5,7 +5,7 @@ export default class Header extends Component {
     render() {
         return (
             <div className="header">
-                <img src="https://ibb.co/9HBF6jH" alt="hello there"/>
+                <img src="https://ibb.co/9HBF6jH" alt="hello"/>
                 <a href="https://youtu.be/I2UBjN5ER4s" className="hidden-msg">
                     <h1>Click Me</h1>
                 </a>

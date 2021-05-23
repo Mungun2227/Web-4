9e9a0888c61e42aeb5f6b112ddfeec54d1944aa2
Mungun2227 <78751955+Mungun2227@users.noreply.github.com>
2021-05-23 15:49:51 +0800

    Add files via upload

---
+++ components/header/Header.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import './Header.css';
+
+export default class Header extends Component {
+    render() {
+        return (
+            <div className="header">
+                <img src="https://ibb.co/9HBF6jH" alt="hello there"/>
+                <a href="https://youtu.be/I2UBjN5ER4s" className="hidden-msg">
+                    <h1>Click Me</h1>
+                </a>
+            </div>
+        )
+    }
+}
